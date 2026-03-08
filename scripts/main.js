5806cb7049ff01698384f70437426dc46fc77dac
mi-araf <arafmushfiq@gmail.com>
2026-03-08 14:45:09 +0600

    api loading

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-console.log('hello, main js');
+const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+fetch(url)
+    .then(res => res.json())
+    .then(allData => totalIssues(allData));
+    
+function totalIssues(allData) {
+    const totalIssues = document.getElementById('total-issues');
+    totalIssues.innerText = allData.data.length;
+}
