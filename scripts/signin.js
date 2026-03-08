f967adb8a71bb8d94c5d18e6c737e1f52ad2f00d
mi-araf <arafmushfiq@gmail.com>
2026-03-08 13:11:39 +0600

    tab section modified

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+document.getElementById('sign-in-btn').addEventListener('click', () => {
+    const userInput = document.getElementById('user-input');
+    const userInputValue = userInput.value;
+    const userPass = document.getElementById('user-pass');
+    const userPassValue = userPass.value;
+
+    if (userInputValue == 'admin' && userPassValue == 'admin123') {
+        window.location.assign('./main.html');
+    } else {
+        alert('Invalid Credentials');
+        return;
+    }
+
+})
